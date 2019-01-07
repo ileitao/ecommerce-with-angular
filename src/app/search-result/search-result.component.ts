209c01b4a027f95b156a4d8c503687d72069d56d
ileitao <ignacio.leitao@gmail.com>
2019-01-07 00:17:46 -0300

    Improve the filters to work in conjunction

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -22,10 +22,15 @@ export class SearchResultComponent implements OnInit, OnDestroy  {
    * @type {Product[]}
    */
   filteredProducts: Product[] = [];
+  isSearchMenuAvailable: boolean = false;
+  isFilterMenuAvailable: boolean = true;
   isAvailabilityValueFiltered: boolean = false;
   isMinValueFiltered: boolean = false;
   isMaxValueFiltered: boolean = false;
   isOnStockAmountFiltered: boolean = false;
+  currentMin: number = 0;
+  currentMax: number = 0;
+  currentStock: number = 0;
   subscription: Subscription;
 	
   constructor(private route: ActivatedRoute, private data: DataService, private filterService: FilterService, private shoppingCartService: ShoppingCartService)  {
@@ -59,6 +64,7 @@ export class SearchResultComponent implements OnInit, OnDestroy  {
     // Reset filters after a new search result
     this.filterService.resetFilters();
     if (id === 0) {
+      this.isSearchMenuAvailable = false;
       this.data.getProducts().subscribe(data => {
         let productsArray = data["products"];
         for(let obj of productsArray) { 
@@ -67,6 +73,7 @@ export class SearchResultComponent implements OnInit, OnDestroy  {
         }
       });
     } else {
+      this.isSearchMenuAvailable = true;
       this.data.getProductsById(id).subscribe(data => {
         for(let obj of data) { 
           this.products.push(new Product(obj["quantity"], obj["price"], obj["available"], obj["sublevel_id"], obj["name"], obj["id"]));
@@ -79,61 +86,151 @@ export class SearchResultComponent implements OnInit, OnDestroy  {
   // FILTERS
   onFilterByAvailability(value: boolean) {
     if (value === true) {
+      this.isAvailabilityValueFiltered = true;
       this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
         return item.available !== false;
       })
     } else if (value === false) {
+      this.isAvailabilityValueFiltered = false;
       this.filteredProducts = Object.assign([], this.products);
+    }
+    this.filterOthersThanAvailability();
+  }
+
+  private filterOthersThanAvailability() {
+    if (this.currentMax > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) < this.currentMax;
+      })
+    }
+    if (this.currentMin > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) > this.currentMin;
+      })
+    }
+    if (this.currentStock > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.quantity > this.currentStock;
+      })
     }
   }
 
   onFilterByPriceMin(value: number) {
+    this.currentMin = value;
     if (value > 0) {
-      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+      this.filteredProducts = Object.assign([], this.products).filter(item => {
         return Number(item.price.replace(/[^0-9.-]+/g,"")) > value;
+      })
+    } else {
+      this.filteredProducts = Object.assign([], this.products);
+    }
+    
+    this.filterOthersThanPriceMin();
+  }
+
+  private filterOthersThanPriceMin() {
+    if (this.isAvailabilityValueFiltered) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.available !== false;
+      })
+    }
+    if (this.currentMax > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) < this.currentMax;
+      })
+    }
+    if (this.currentStock > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.quantity > this.currentStock;
       })
     }
   }
 
   onFilterByPriceMax(value: number) {
-    if (value > 0) {      
-      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+    this.currentMax = value;
+    if (value > 0) {
+      this.filteredProducts = Object.assign([], this.products).filter(item => {
         return Number(item.price.replace(/[^0-9.-]+/g,"")) < value;
+      })
+    } else {
+      this.filteredProducts = Object.assign([], this.products);
+    }
+
+    this.filterOthersThanPriceMax();
+  }
+
+  private filterOthersThanPriceMax() {
+    if (this.isAvailabilityValueFiltered) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.available !== false;
+      })
+    }
+    if (this.currentMin > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) > this.currentMin;
+      })
+    }
+    if (this.currentStock > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.quantity > this.currentStock;
       })
     }
   }
 
   onFilterByStockAmount(value: number) {
+    this.currentStock = value;
     if (value > 0) {
       this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
         return item.quantity > value;
       })
+    } else {
+      this.filteredProducts = Object.assign([], this.products);
     }
+
+    this.filterOthersThanStockAmount();
+  }
+
+  private filterOthersThanStockAmount() {
+    if (this.isAvailabilityValueFiltered) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return item.available !== false;
+      })
+    }
+    if (this.currentMin > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) > this.currentMin;
+      })
+    }
+    if (this.currentMax > 0) {
+      this.filteredProducts = Object.assign([], this.filteredProducts).filter(item => {
+        return Number(item.price.replace(/[^0-9.-]+/g,"")) < this.currentMax;
+      })
+    }
+  }
+
+  onFilterByName(name: string) {
+    const regexp = new RegExp(name, 'i');
+    this.filteredProducts = Object.assign([], this.products.filter(x => regexp.test(x.name)));
   }
 
   // SORTERS
   onSortByAvailability(value: boolean) {
     this.filteredProducts = Object.assign([], this.filteredProducts.sort((a, b) => a.available === value ? 1 : -1));
-    console.log(this.filteredProducts)
   }
 
   onSortByPrice(value: boolean) {
     if (value) {
       this.filteredProducts = Object.assign([], this.filteredProducts.sort((a, b) => Number(a.price.replace(/[^0-9.-]+/g,"")) - Number(b.price.replace(/[^0-9.-]+/g,""))));
-      console.log(this.filteredProducts)
     } else {
       this.filteredProducts = Object.assign([], this.filteredProducts.sort((a, b) => Number(b.price.replace(/[^0-9.-]+/g,"")) - Number(a.price.replace(/[^0-9.-]+/g,""))));
-          console.log(this.filteredProducts)
     }
   }
 
   onSortByStockAmount(value: boolean) {
     if (value) {
       this.filteredProducts = Object.assign([], this.filteredProducts.sort((a, b) => a.quantity - b.quantity));
-      console.log(this.filteredProducts)
     } else {
       this.filteredProducts = Object.assign([], this.filteredProducts.sort((a, b) => b.quantity - a.quantity));
-      console.log(this.filteredProducts)
     }
   }
 
